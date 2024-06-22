73feb60c38edf346c555579c704b5bc226ad4583
8Mostafa0 <mosielite@gmail.com>
2024-06-22 12:27:46 +0330

    `Test_Mikonim_`

--- git automation.py
+++ git automation.py
@@ -4,13 +4,33 @@ import shutil
 
 
 
-def main():
-    
+
+def copy_file():
+    shutil.copy('./test.txt','./1text1.txt')
+
+
+def rem_file():
+    os.remove('./1test.txt')
+
+
+
+
+def push_commit(commit_message):
+    commit_message=commit_message.split(" ")
+    text = ""
+    for i in commit_message:
+        text+= i+"_"
+    print(text)
+    os.system(f'cmd /c "git commit -m `{commit_message}`"')
+    os.system('cmd /c "git push"') 
     os.system('cmd /c "git init"')
-    shutil.copy('./test.txt','./1test.txt')
     os.system('cmd /c "git add ."')
-    os.system('cmd /c "git commit -m `mosielite`"')
-    os.system('cmd /c "git push"')
-     
+
+
+
+
+def main():
+    copy_file()     
+    push_commit("Test Mikonim")     
 if __name__ == '__main__':
     main()
